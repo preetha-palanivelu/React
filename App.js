258d17b9585ff2e24ba5448a8c373cba70f8662b
Preetha <preethajivitam@gmail.com>
2024-10-15 12:33:07 +0530

    react component created

--- App.js
+++ App.js
@@ -1,26 +1,46 @@
-// const heading = React.createElement(
-//   "h1",
+import React from "react";
+import ReactDom from "react-dom/client";
+
+// ! using React.createlement
+// const header = React.createElement(
+//   "div",
 //   { id: "heading" },
-//   "hello from react world"
+//   React.createElement(
+//     "h1",
+//     { id: "tag" },
+//     "hello i am H1 tag",
+//     React.createElement("h2", { id: "tag2" }, "hello i am H2 tag")
+//   )
 // );
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-    React.createElement("div", { id: "child1 " }, [
-      React.createElement("h1", {}, "I am in H1 tagg"),
-      React.createElement("h1", {}, "I am in H1 tagg"),
-    ]),
-  ],
-  [
-    React.createElement("div", { id: "child2 " }, [
-      React.createElement("h1", {}, "I am in H1 tagg"),
-      React.createElement("h1", {}, "I am in H1 tagg"),
-    ]),
-  ]
+//!using JSX
+
+const jsxHeader = (
+  <div id="heading">
+    <h1 id="h1tag">Hello I ma in h1 tag</h1>
+    <h2 id="h2tag">Hello I am in h2 tag</h2>
+  </div>
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+//!using functional component
 
-root.render(parent);
+const Component2 = () => (
+  <div id="second">
+    <h1 className="sec">I am in second component</h1>
+  </div>
+);
+
+const JsxHeaader = () => (
+  <div id="head" className="honh">
+    {jsxHeader}
+    <Component2 />
+    <h1 id="ta">I am in functional component1</h1>
+    <h2 id="taa">I am in functional component2</h2>
+  </div>
+);
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+// root.render(jsxHeader);
+
+root.render(<JsxHeaader />);
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-#React Practice
+# React Practice
+
+# Parcel
+
+- Dev build
+- Local server
+- HMR=Hot Module Replacement
+- File watching algorithm- written in C++
